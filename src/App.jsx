143320872a66c2f0eec264bdcdd16baf872f2368
ARUNKUMARMARU <arunsmart1020@gmail.com>
2023-10-18 12:57:10 +0530

    initial

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,41 @@
-const Sample = () => {
-  return(
-    <div>
-      <p>This is sample text</p>
-    </div>
-  )
-}
+// const Sample = () => {
+//   return(
+//     <div>
+//       <p>This is sample text</p>
+//     </div>
+//   )
+// }
 
-const app = () => {
-  const date = new Date(); 
-  return(
-    <div>
-      <p>Hello world..! today's date is {date.toString()}</p>
-      < Sample />
-    </div>
-  )
-}
+// const app = () => {
+//   const date = new Date(); 
+//   return(
+//     <div>
+//       <p>Hello world..! today's date is {date.toString()}</p>
+//       < Sample />
+//     </div>
+//   )
+// }
 
-export default app;
+import React from 'react'
+
+function App(props) {
+  console.log(props)
+    const {notes} = props;
+    console.log(notes)
+        return(
+          <div>
+        <h3>Content</h3>
+        <ul>
+        {
+    notes.map((data) => {
+      return <li key = { notes.id}>{ data.content}</li>
+    })
+     }
+        </ul>
+  </div>
+        )
+};
+
+export default App
+
+
--- src/main.jsx
+++ src/main.jsx
@@ -2,4 +2,30 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 
-ReactDOM.createRoot(document.getElementById('root')).render(<App />)
+const notes = [
+    {id:1,
+     content: "first",
+     importent: true,   
+    },
+    {
+        id:2,
+        content:"second",
+        importent:false,
+    },
+    {
+        id:3,
+        content: "third",
+        importent: true,   
+    },
+    {
+        id:4,
+        content: "fourth",
+        importent: false,   
+       },
+       {
+        id:5,
+        content: "fifth",
+        importent: true,   
+       },
+];
+ReactDOM.createRoot(document.getElementById('root')).render(<App notes={notes} />)
